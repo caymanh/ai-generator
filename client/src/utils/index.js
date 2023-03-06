b4a10d41b3adaca7c818f15005fb79eca4786779
Cayman Heng <hengcayman@gmail.com>
2023-03-06 17:28:29 +0800

    updated Create Post page and utils

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,10 +1,10 @@
 import { surpriseMePrompts } from "../constants";
 
-export function getRandomPrompts(prompt) {
+export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
   //call the function again if the random prompt is the same as the previous random prompt
-  if (randomPrompt === prompt) return getRandomPrompts(prompt);
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
 }
