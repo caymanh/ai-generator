e6e8452a01f2b4c6233982897f9529da2df8fe3d
Cayman Heng <hengcayman@gmail.com>
2023-03-06 17:48:06 +0800

    updated handle submit function on the create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -58,7 +58,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "https://dalle-arbb.onrender.com/api/v1/post",
+          "http://localhost:8080/api/v1/post",
           {
             method: "POST",
             headers: {
@@ -70,6 +70,7 @@ const CreatePost = () => {
 
         await response.json();
         alert("Success");
+        //go back to home to see the image
         navigate("/");
       } catch (err) {
         alert(err);
