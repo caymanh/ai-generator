54be216916b097aae35458678d5b09280d3ecd99
Cayman Heng <hengcayman@gmail.com>
2023-03-06 17:28:01 +0800

    fixed dalle route

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -13,10 +13,9 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-  res.send("Hello from DALL-E!");
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
-//make it async because it will take some time
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
@@ -29,11 +28,12 @@ router.route("/").post(async (req, res) => {
     });
 
     const image = aiResponse.data.data[0].b64_json;
-
-    req.status(200).json({ photo: image });
+    res.status(200).json({ photo: image });
   } catch (error) {
-    console.log(error);
-    res.status(500).send(error?.response.data.error.message);
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
   }
 });
 
