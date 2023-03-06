03e2b736bb53919700c9dbe6eecfe863622b707e
Cayman Heng <hengcayman@gmail.com>
2023-03-06 22:06:37 +0800

    updated pages and Card component

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -29,7 +29,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
+        const response = await fetch("https://ai-image-generator-g3vu.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -58,7 +58,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "http://localhost:8080/api/v1/post",
+          "https://ai-image-generator-g3vu.onrender.com/api/v1/post",
           {
             method: "POST",
             headers: {
